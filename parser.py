38d30b889f313daa59f0e11b9f4618f4007b7a88
Omarf8 <omarfigueroa223@gmail.com>
2026-03-17 11:57:46 -0500

    Query Google Gemini API for important dates

--- parser.py
+++ parser.py
@@ -1,7 +1,19 @@
+from dotenv import load_dotenv
 import pdfplumber
+from google import genai
 
+load_dotenv()
+
+client = genai.Client()
 data = ""
 
-with pdfplumber.open("Math_218_Syllabus_Spring_2026.pdf") as pdf:
+with pdfplumber.open("FILEPATH") as pdf:
     for page in pdf.pages:
         data += f"\n{page.extract_text()}"
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview", 
+    contents=f"Using the following data, I want you to give me back the defined (not TBD) important dates listed such as homework, exams, and/or quizzes. Your response should be formatted as 'COURSE - TYPE DATE': \n{data}"
+)
+
+print(response.text)
