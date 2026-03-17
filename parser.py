f4ce0f041b265c4f37d14dc06c9088deb0321f79
Omarf8 <omarfigueroa223@gmail.com>
2026-03-17 12:48:00 -0500

    Correctly Query for a JSON array of data

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import json
 import pdfplumber
 from google import genai
 
@@ -7,13 +8,15 @@ load_dotenv()
 client = genai.Client()
 data = ""
 
-with pdfplumber.open("FILEPATH") as pdf:
+with pdfplumber.open("FILEPATH.pdf") as pdf:
     for page in pdf.pages:
         data += f"\n{page.extract_text()}"
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview", 
-    contents=f"Using the following data, I want you to give me back the defined (not TBD) important dates listed such as homework, exams, and/or quizzes. Your response should be formatted as 'COURSE - TYPE DATE': \n{data}"
+    contents=f"Using the following data, I want you to give me back the defined (not TBD) important dates listed such as homework, exams, quizzes, and/or any other types of assignments. Return the raw JSON with no markdown. Your response should be a JSON array where we want the title of the assignment, the type (HOMEWORK, EXAM, etc.), date (YYYY-MM-DD), and the course (NAME NUMBER): {data}"
 )
 
-print(response.text)
+json_list_raw_text = response.text
+json_list_text = json_list_raw_text.replace("```json", "").replace("```", "") # Potential md stripping
+print(json_list_text)
