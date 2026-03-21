14475b8322768ccba267bc148cfd51581234e753
Omarf8 <omarfigueroa223@gmail.com>
2026-03-21 11:21:56 -0500

    Fast API Post for Syllabus Parsing

--- parser.py
+++ parser.py
@@ -1,23 +1,48 @@
 from dotenv import load_dotenv
 import json
 import pdfplumber
+import io
 from google import genai
+from fastapi import FastAPI, UploadFile
 
 load_dotenv()
 
+app = FastAPI()
+
 client = genai.Client()
-data = ""
 
-with pdfplumber.open("FILEPATH.pdf") as pdf:
-    for page in pdf.pages:
-        data += f"\n{page.extract_text()}"
+@app.post("/uploadfile/")
+async def parse_syllabus(file: UploadFile):
+    rawBytes = await file.read()
+    syllabus = io.BytesIO(rawBytes)
 
-response = client.models.generate_content(
-    model="gemini-3-flash-preview", 
-    contents=f"Using the following data, I want you to give me back the defined (not TBD) important dates listed such as homework, exams, quizzes, and/or any other types of assignments. Return the raw JSON with no markdown. Your response should be a JSON array where we want the title of the assignment, the type (HOMEWORK, EXAM, etc.), date (YYYY-MM-DD), and the course (NAME NUMBER): {data}"
-)
+    data = ""
+    with pdfplumber.open(syllabus) as pdf:
+        for page in pdf.pages:
+            data += f"\n{page.extract_text()}"
 
-json_list_raw_text = response.text
-json_list_text = json_list_raw_text.replace("```json", "").replace("```", "") # Potential md stripping
-json_py = json.loads(json_list_text)
-print(json_py)
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview", 
+        contents=f"Using the following data, I want you to give me back the defined (not TBD) important dates listed such as homework, exams, quizzes, and/or any other types of assignments. Return the raw JSON with no markdown. Your response should be a JSON array where we want the title of the assignment, the type (HOMEWORK, EXAM, etc.), date (YYYY-MM-DD), and the course (NAME NUMBER): {data}"
+    )
+
+    gemini_text = response.text
+    json_text = gemini_text.replace("```json", "").replace("```", "") # Potential md stripping
+    return json.loads(json_text)
+
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: bool | None = None
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str | None = None):
+#     return {"item_id": item_id, "q": q}
+
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
